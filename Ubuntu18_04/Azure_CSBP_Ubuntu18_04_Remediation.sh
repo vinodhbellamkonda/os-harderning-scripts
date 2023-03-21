3ed5f4f877d1d8cb7abc5ffc5a79a0bc15827f9f
vinodhbellamkonda <108060329+vinodhbellamkonda@users.noreply.github.com>
2023-03-21 15:57:16 +0530

    Update Azure_CSBP_Ubuntu18_04_Remediation.sh

--- Ubuntu18_04/Azure_CSBP_Ubuntu18_04_Remediation.sh
+++ Ubuntu18_04/Azure_CSBP_Ubuntu18_04_Remediation.sh
@@ -301,6 +301,15 @@ else
     fail=$((fail + 1))
 fi
 
+#Ensure source routed packets are not accepted
+echo
+echo -e "${RED}3.2.1${NC} Ensure source routed packets are not accepted"
+egrep -q "^(\s*)net.ipv4.conf.all.accept_source_route\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.all.accept_source_route\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.all.accept_source_route = 0\2/" /etc/sysctl.conf || echo "net.ipv4.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
+egrep -q "^(\s*)net.ipv4.conf.default.accept_source_route\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)net.ipv4.conf.default.accept_source_route\s*=\s*\S+(\s*#.*)?\s*$/\1net.ipv4.conf.default.accept_source_route = 0\2/" /etc/sysctl.conf || echo "net.ipv4.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
+echo -e "${GREEN}Remediated:${NC} Ensure source routed packets are not accepted"
+success=$((success + 1)
+
+
 ###########################################################################################################################
 
 echo
